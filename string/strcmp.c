5dd5f750032fbd78d72ca729ec8e35855d7ff051
wxudongCode <wxudong1991@163.com>
2015-08-30 10:46:28 +0800

    string/strcmp.c

---
+++ string/strcmp.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<assert.h>
+
+int strcmp(const char *str1, const char *str2)
+{
+	while(*str1 == *str2)
+	{
+		if(*str1 == '\0')  //两个字符串都到末尾 
+			return 0;
+		str1++;
+		str2++;
+	}
+	return *str1 - *str2;
+}
+
+int main()
+{
+	assert(strcmp("abc","abc") == 0);
+	assert(strcmp("ab","abc") < 0); // '\0' = 0
+	assert(strcmp("abc","aba") > 0);
+	return 0;
+}
+
